b79276194efab3cc866c0951b63f7d734a19b552
Kevin Hanson <ziffoit@gmail.com>
2021-06-27 16:13:17 -0600

    stats endpoint and totals

--- controllers/api/exercise.js
+++ controllers/api/exercise.js
@@ -23,12 +23,24 @@ router.put("/:id", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-    const fitness = new Fitness({exercises: [req.body]});
+    const fitness = new Fitness({ exercises: [req.body] });
     fitness.save(function (err) {
-      if (err) {
-        res.status(400).json(err);
-      }
+        if (err) {
+            res.status(400).json(err);
+        }
     });
+});
+
+router.get("/range", (req, res) => {
+    Fitness.find()
+        .sort({ date: -1 })
+        .limit(7)
+        .then((exercises) => {
+            res.json(exercises);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
 });
 
 module.exports = router;
